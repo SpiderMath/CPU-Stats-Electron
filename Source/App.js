3424fccbb4abe13e678796b508483d8c72a9b765
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-07-09 23:50:02 +0530

    Made the base app, and will work with WebSockets, since they are easier than setting up Electrongit clone https://github.com/SpiderMath/CPU-Stats-Electrongit clone https://github.com/SpiderMath/CPU-Stats-Electron!

---
+++ Source/App.js
@@ -0,0 +1,61 @@
+const { app, BrowserWindow, Menu } = require("electron");
+const { join } = require("path");
+const { config } = require("dotenv");
+config();
+
+console.customLog = function(string) {
+	this.log(`${new Date().toUTCString()} ${string}`);
+};
+
+function isDarwin() {
+	return process.platform === "darwin";
+}
+
+app.on("ready", () => {
+	console.customLog("The app is ready!");
+	const mainWindow = new BrowserWindow({
+		height: 800,
+		width: 800,
+		center: true,
+		webPreferences: {
+			nodeIntegration: true,
+		},
+	});
+
+	mainWindow.loadURL(`file://${join(__dirname, "./Public/Index.html")}`);
+
+	const customMenu = Menu.buildFromTemplate([
+		{
+			// label creates a menu for us..
+			label: "Options",
+			// Menu inside a menu
+			submenu: [
+				{
+					// Label of the option of the menu
+					label: "Quit",
+					// Function to work with what happens on click
+					click() {
+						app.quit();
+					},
+					// Hotkey stuff, nice naming
+					accelerator: `${isDarwin() ? "Command" : "Ctrl"}+Q`,
+				},
+				{
+					label: "Reload",
+					click() {
+						return mainWindow.reload();
+					},
+					accelerator: `${isDarwin() ? "Command" : "Ctrl"}+R`,
+				},
+			],
+		},
+	]);
+
+	mainWindow.setMenu(customMenu);
+
+	mainWindow.on("close", () => {
+		app.quit();
+	});
+
+	if(process.env.NODE_ENV !== "production") mainWindow.webContents.openDevTools();
+});
